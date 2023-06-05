4718a87de964444db2e2b2fd721c0cb3393ad1aa
APAC\UUU6KOR <Subramanya.Gurudath@in.bosch.com>
2023-06-05 15:44:17 +0530

    updated files

--- src/app/speakers/survey/survey.component.ts
+++ src/app/speakers/survey/survey.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-survey',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class SurveyComponent {
 
+  getSurveyUrl="https://bootcamp23.azurewebsites.net/getSurveyResults";
+  questions = []
+  question = {}
+  show = false;
+  constructor(private http: HttpClient) { }
+  ngOnInit(){
+    this.http.get(this.getSurveyUrl).subscribe((data) =>{
+      for(let question in data["feedbackResults"]){
+      this.question["question"] = question["Q"];
+      this.question["options"] = question["options"];
+      this.questions.push(this.question);
+    }
+    });
+    this.show = true;
+  }
 }
